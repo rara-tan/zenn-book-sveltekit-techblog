212fee84d57a9a3d84f08288c5912279040eb4ea
rara-tan <barasan1119@gmail.com>
2024-02-10 08:50:50 +0900

    add tailwind CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
-<header>
-  <nav>
-    <a href="/">home</a>
-    <a href="/profiles">Profile</a>
+<script>
+  import "../app.css";
+</script>
+
+<header class="flex items-center justify-between px-6 py-4 bg-black">
+  <h1 class="text-2xl font-bold text-white">Tech Blog</h1>
+  <nav class="flex gap-4">
+    <a href="/" class="text-white hover:text-gray-400">home</a>
+    <a href="/profiles" class="text-white hover:text-gray-400">Profile</a>
   </nav>
 </header>
 
 <slot />
 
-<footer>
+<footer class="bg-black py-2 text-center text-white">
   <p>My Awesome Website!</p>
 </footer>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'], // ここを追加
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
