d5041319afb118ba0925972895e33764f6663052
rara-tan <barasan1119@gmail.com>
2024-02-10 11:15:20 +0900

    add blogpage

--- src/routes/articles/[articleId]/+page.server.js
+++ src/routes/articles/[articleId]/+page.server.js
@@ -1,5 +1,30 @@
-export function load({ params }) {
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+import markdown from 'markdown-it'; // 外部ライブラリである markdown-it を使用してマークダウンをHTMLに変換
+
+const readFile = promisify(fs.readFile);
+
+export async function load({ params }) {
+	// パラメータからファイル名を取得
+	const { articleId } = params;
+	// ファイルパスを作成
+	const filePath = path.resolve('articles', `${articleId}.md`);
+
+	// ファイルを読み込む
+	let fileContent;
+	try {
+		fileContent = await readFile(filePath, 'utf-8');
+	} catch (err) {
+		console.error('Error reading file:', err);
+	}
+
+	const mdParser = new markdown();
+	const htmlContent = mdParser.render(fileContent || ''); // マークダウンをHTMLに変換
+
+	// 枠組みに提供するデータを返す
 	return {
-		params
+		params,
+		htmlContent // 各マークダウンの中身
 	};
 }
--- src/routes/articles/[articleId]/+page.svelte
+++ src/routes/articles/[articleId]/+page.svelte
@@ -3,3 +3,6 @@
 </script>
 
 <h1>{data.params.articleId}</h1>
+
+<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+<div class="content">{@html data.htmlContent}</div>
