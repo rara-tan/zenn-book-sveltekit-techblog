99674f280db6020ca83eed4cba2ea82867aa212f
rara-tan <barasan1119@gmail.com>
2024-02-10 09:51:35 +0900

    format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,17 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
 <header class="flex items-center justify-between px-6 py-4 bg-black">
-  <h1 class="text-2xl font-bold text-white">Tech Blog</h1>
-  <nav class="flex gap-4">
-    <a href="/" class="text-white hover:text-gray-400">Top</a>
-    <a href="/profiles" class="text-white hover:text-gray-400">Profile</a>
-  </nav>
+	<h1 class="text-2xl font-bold text-white">Tech Blog</h1>
+	<nav class="flex gap-4">
+		<a href="/" class="text-white hover:text-gray-400">Top</a>
+		<a href="/profiles" class="text-white hover:text-gray-400">Profile</a>
+	</nav>
 </header>
 
 <slot />
 
 <footer class="bg-black py-2 text-center text-white">
-  <p>My Awesome Website!</p>
+	<p>My Awesome Website!</p>
 </footer>
