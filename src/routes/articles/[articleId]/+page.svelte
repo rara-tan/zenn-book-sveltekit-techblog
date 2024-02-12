ced72a9d76560b525cb5bfc72932dfb4dc1b8622
rara-tan <barasan1119@gmail.com>
2024-02-12 09:29:46 +0900

    Article Detail Css

--- src/routes/articles/[articleId]/+page.svelte
+++ src/routes/articles/[articleId]/+page.svelte
@@ -2,9 +2,17 @@
 	export let data;
 </script>
 
-<h1>{data.metadata.title}</h1>
-<p>{data.metadata.category}</p>
-<p>{data.metadata.date}</p>
+<div class="py-8 px-4 mb-10 rounded-lg flex flex-col items-center bg-slate-100">
+	<h1 class="text-3xl font-bold">{data.metadata.title}</h1>
+	<div class="flex justify-center items-center w-full mt-4">
+		<p
+			class="inline-flex items-center px-2 py-1 bg-indigo-100 text-indigo-800 rounded-lg text-sm mr-2"
+		>
+			{data.metadata.category}
+		</p>
+		<p class="text-lg text-gray-600 ml-2 text-s">{data.metadata.date}</p>
+	</div>
+</div>
 
 <!-- eslint-disable-next-line svelte/no-at-html-tags -->
 <article class="markdown-body">{@html data.htmlContent}</article>
