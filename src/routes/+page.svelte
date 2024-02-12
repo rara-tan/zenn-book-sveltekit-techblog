c1554385afbb896933daeeee2e09bc1e257bc107
rara-tan <barasan1119@gmail.com>
2024-02-12 11:01:52 +0900

    decorate top page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,15 @@
 	<meta name="description" content="This tech blog features many wonderful articles." />
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>テスト</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="py-8 px-4 mb-10 rounded-lg flex flex-col items-center bg-slate-100">
+	<h1 class="text-3xl font-bold">My Awesome Tech Blog!</h1>
+	<div class="mt-4">
+		<p class="text-lg text-gray-600 text-s">
+			This is my tech blog. Packed with tech articles that serve as handy references for software
+			engineers. Feel free to bookmark if you like what you see!
+		</p>
+	</div>
+</div>
 
 <ul>
 	{#each data.articles as article}
